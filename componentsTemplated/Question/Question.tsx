eb21d551d1bd116c407c6cd2467ba37ecf64d681
Natalie Tan <natalieytan@gmail.com>
2021-04-15 17:44:19 +1000

    style: Update Question template style

--- componentsTemplated/Question/Question.tsx
+++ componentsTemplated/Question/Question.tsx
@@ -1,9 +1,14 @@
 import { Typography, Container } from '@material-ui/core';
 import styled from 'styled-components';
 
+const OuterContainer = styled.div`
+  min-height: 550px;
+  padding: 4rem 0;
+  width: 100%;
+`;
+
 const QuestionContainer = styled(Container).attrs({ maxWidth: 'md' })`
   text-align: center;
-  padding: 1.5rem 0;
 `;
 
 const QuestionBody = styled.div`
@@ -19,11 +24,11 @@ export default function Question(props: Props): JSX.Element {
   const { title, children } = props;
 
   return (
-    <Container maxWidth={false}>
+    <OuterContainer>
       <QuestionContainer>
         <Typography variant="h5">{title}</Typography>
         <QuestionBody>{children}</QuestionBody>
       </QuestionContainer>
-    </Container>
+    </OuterContainer>
   );
 }
